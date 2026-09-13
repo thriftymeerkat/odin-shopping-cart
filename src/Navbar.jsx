a4672733594ef04df2555e095ad525933741718c
mjcw88 <144539369+mjcw88@users.noreply.github.com>
2026-09-13 23:28:17 +0100

    added basic styling to navbar layout

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,17 +4,14 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div>
-      <h1 className="shop-name">Odin Jeans</h1>
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/"><h1 className="shop-name">Odin Jeans</h1></Link>
           </li>
           <li>
-            <Link to="/jeans">Jeans</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
+              <Link to="/jeans">Jeans</Link>
+              <Link to="/cart">Cart</Link>
           </li>
         </ul>
       </nav>
